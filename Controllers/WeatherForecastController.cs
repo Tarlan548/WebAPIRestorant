70c3e3566789f15eccf2a7c2ba417a6b3d026884
Tarlan548 <75043664+Tarlan548@users.noreply.github.com>
2023-11-14 20:19:53 +0400

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace WebAPIRestorant.Controllers
         private static readonly string[] Summaries = new[]
         {
         
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Tarlan"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Tarlan", "Ulvi" 
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
